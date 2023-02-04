9ded10fd1949ed2366eb94af75127698bcb9a98e
qdkhan <qutbuddin.khan@xipetech.com>
2023-02-04 15:40:53 +0530

    Ajax Validation

---
+++ public/assets/js/custom.js
@@ -0,0 +1,16 @@
+function validateForm(id) {
+    'use strict'
+    var forms = document.querySelectorAll(`#${id}`)
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('click', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+    })
+};
